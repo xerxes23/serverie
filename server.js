fc544d8de11b523e9ef5194bf2916cd555ebb320
xerxes23 <josephmendoza23@gmail.com>
2018-08-21 10:58:53 -0400

    Wrote and implemented simple express middlewares

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const hbs = require("hbs");
+
+// Initializing Express app
 const app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
+
 hbs.registerHelper("getCurrentYear", () => {
   return new Date().getFullYear();
 });
+
 hbs.registerHelper("screamIt", text => {
   return text.toUpperCase();
 });
 
 app.set("view engine", "hbs");
+
 app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
 
 app.get("/", (req, res) => {
   res.render("home.hbs", {
