c19323c4b083c54937e6edd3a2663a53d8cdf19e
xerxes23 <josephmendoza23@gmail.com>
2018-08-20 20:58:59 -0400

    Introduced Handlebars, partials, and helpers to the application

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.json({
-    name: "Joseph Mendoza",
-    likes: ["Node", "Python", "Machine Learning", "MongoDB"],
-    age: 26
+  res.render("home.hbs", {
+    pageTitle: "Home",
+    welcomeMessage: "Welcome to my website"
   });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
 });
 
 app.get("/bad", (req, res) => {
